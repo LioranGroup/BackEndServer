06c5d2ee1ec9f241f76f9f89389455408ac16827
Iori <esqcorp3@gmail.com>
2025-05-26 20:50:57 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./DataBase/database.db', (err) => {
+  if (err) {
+    console.error('Error al conectar con la base de datos:', err.message);
+  } else {
+    console.log('Conectado a la base de datos SQLite.');
+  }
+});
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando localmente');
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+  res.send(`Hola, ${req.params.nombre}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+
+
+db.serialize(() => {
+  db.each("SELECT name FROM sqlite_master WHERE type='table'", (err, row) => {
+    if (err) {
+      console.error('Error al listar tablas:', err.message);
+    } else {
+      console.log(`Tabla encontrada: ${row.name}`);
+    }
+  });
+});
+
